da763f2b9bc50b7e1fe02c7e1cc5919780e072bd
Nicolay Belyaev <nickb2147@gmail.com>
2022-06-21 21:28:10 +0300

    Favorite name (if-else)

---
+++ Example005_FavoriteName/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "вася")
+{
+    Console.WriteLine("Ура, это Вася!");
+}
+else
+{
+    Console.WriteLine("Привет");
+}
