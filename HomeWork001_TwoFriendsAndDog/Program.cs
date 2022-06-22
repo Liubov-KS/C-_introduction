ce678fe7b98aa4927b5a481d9667b944b6254c5b
Nicolay Belyaev <nickb2147@gmail.com>
2022-06-22 13:34:06 +0300

    Friends

---
+++ HomeWork001_TwoFriendsAndDog/Program.cs
@@ -0,0 +1,24 @@
+﻿int FirstFriendSpeed = 5;
+int SecondFriendSpeed = 7;
+int DogSpeed = 15;
+int DistanceBetweenFriends = 1000;
+int FinalDistance = 100;
+int FriendsCounter = 0;
+int RunsCounter = 0;
+
+while(DistanceBetweenFriends > FinalDistance)
+{
+    if (FriendsCounter == 0)
+    {
+        DistanceBetweenFriends = DistanceBetweenFriends - ((FirstFriendSpeed+SecondFriendSpeed) * (DistanceBetweenFriends/(FirstFriendSpeed+DogSpeed)));
+        RunsCounter ++;
+        FriendsCounter = 1;
+    }
+    if (FriendsCounter == 1)
+    {
+        DistanceBetweenFriends = DistanceBetweenFriends - ((FirstFriendSpeed+SecondFriendSpeed) * (DistanceBetweenFriends/(SecondFriendSpeed+DogSpeed)));
+        RunsCounter ++;
+        FriendsCounter = 0;
+    }
+}
+Console.WriteLine(RunsCounter);
