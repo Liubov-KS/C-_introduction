c0a311552ddb5715ec6512fd2d192a3eec255d9e
Nicolay Belyaev <nickb2147@gmail.com>
2022-06-22 21:40:29 +0300

    Good boy!

--- HomeWork001_TwoFriendsAndDog/Program.cs
+++ HomeWork001_TwoFriendsAndDog/Program.cs
@@ -1,8 +1,8 @@
-﻿int FirstFriendSpeed = 5;
-int SecondFriendSpeed = 7;
-int DogSpeed = 15;
-int DistanceBetweenFriends = 1000;
-int FinalDistance = 100;
+﻿float FirstFriendSpeed = 5;
+float SecondFriendSpeed = 7;
+float DogSpeed = 15;
+float DistanceBetweenFriends = 1000;
+int FinalDistance = 5;
 int FriendsCounter = 0;
 int RunsCounter = 0;
 
@@ -12,12 +12,14 @@ while(DistanceBetweenFriends > FinalDistance)
     {
         DistanceBetweenFriends = DistanceBetweenFriends - ((FirstFriendSpeed+SecondFriendSpeed) * (DistanceBetweenFriends/(FirstFriendSpeed+DogSpeed)));
         RunsCounter ++;
+        if (DistanceBetweenFriends <= FinalDistance) break; // если после очередной пробежки от 1 друга к 2 дистанция уже меньше минимально, не надо бежать еще раз
         FriendsCounter = 1;
     }
     if (FriendsCounter == 1)
     {
         DistanceBetweenFriends = DistanceBetweenFriends - ((FirstFriendSpeed+SecondFriendSpeed) * (DistanceBetweenFriends/(SecondFriendSpeed+DogSpeed)));
         RunsCounter ++;
+        if (DistanceBetweenFriends <= FinalDistance) break; // мы не знаем, с какого друга может начинатся игра и быть может они уже в упор друг другу
         FriendsCounter = 0;
     }
 }
