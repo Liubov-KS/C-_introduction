13732a1609963430962011f2ba7661b1f6526bd2
Nicolay Belyaev <nickb2147@gmail.com>
2022-06-23 17:05:03 +0300

    DayOfTheWeek

---
+++ Seminar.01.002_DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите номер дня: ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+var days = new Dictionary<int, string>()
+{
+    {1, "понедельник"},
+    {2, "вторник"},
+    {3, "среда"},
+    {4, "четверг"},
+    {5, "пятница"},
+    {6, "суббота"},
+    {7, "воскресенье"}
+};
+if (days.ContainsKey(daynumber) == true)
+{
+    Console.Write($"{daynumber} день недели - это {days[daynumber]}");
+}
+else
+{
+    Console.Write("В неделе 7 дней. Введите число от 1 до 7.");
+}
--- Seminar000_Square/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿Console.Write("Введите число, которое нужно возвести в квадрат: ");
-Console.WriteLine($"Результат: {Math.Pow(double.Parse(Console.ReadLine()), 2)}");
