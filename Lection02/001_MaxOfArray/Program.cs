589e91bfb2858c7bd851ba9d04c28aaf487768d3
Nicolay Belyaev <nickb2147@gmail.com>
2022-06-25 13:44:17 +0300

    new catalog structure

--- Lection02/001_MaxOfArray/Program.cs
+++ Lection02/001_MaxOfArray/Program.cs
@@ -3,14 +3,13 @@
     int result = array[0];
     foreach (int i in array)
     {
-        if (array[i] > result)
+        if (i > result)
         {
-            result = array[i];
+            result = i;
         }
     }
     return result;   
 }
-    
 
-int[] array = {11,32,34,45,56,67,72,84,92};
+int[] array = {11, 32, 34, 45, 56, 67, 72, 84, 92};
 Console.Write(Max(array));
