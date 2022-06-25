bec7f074590810114d502b8fe56e7cd138a646fb
Nicolay Belyaev <nickb2147@gmail.com>
2022-06-25 14:09:10 +0300

    MethodArray

---
+++ Lection02/002_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int elem = Convert.ToInt32(Console.ReadLine());
+int[] Massive ={1, 2, 3, 4, 5, 6, 7, 8};
+
+int ElemFind(int[] array, int elem)
+{
+    foreach (int i in array)
+    {
+        if (i == elem)
+        {
+            Console.Write($"{elem} есть в массиве");
+            break;
+        }
+        Console.Write($"{elem} не найден в массиве");    
+    }
+    return;
+}
+
+ElemFind(Massive, elem);
