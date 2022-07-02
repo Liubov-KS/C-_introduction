5f46681f64c0530a7ed2168e0b7c2e4109597998
Nicolay Belyaev <nickb2147@gmail.com>
2022-07-02 14:30:03 +0300

    Lection03

---
+++ Lection03/000_VoidMethod/Program.cs
@@ -0,0 +1,56 @@
+﻿// Метод, который ничего не принимает и не возвращает.
+
+void Method1()
+{
+    Console.WriteLine("Автор программы Николай Беляев");
+}
+
+Method1();
+
+// Метод, который что-то принимает, но ничего не возвращает.
+
+void Method2(string msg)
+{
+    Console.WriteLine($"Автор программы {msg}");
+}
+
+Method2("Иван Петров");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine($"Это сообщение: {msg} выведено на экран {count} раз");
+        i++;
+    }
+}
+
+Method21("Помой посоду", 3);
+
+// Методы, которые ничего не принимают, но что-то возвращают.
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// Методы, которые что-то принимают и что-то возвращают.
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result += text;
+        i++;
+    }
+    return result;
+}
+
+string result = Method4(10, "Zzz");
+Console.WriteLine(result);
---
+++ Lection03/001_For/Program.cs
@@ -0,0 +1,25 @@
+﻿// Циклы - взаимозаменяемы.
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result += text;
+    }
+    return result;
+}
+
+string result = Method4(10, "Zzz");
+Console.WriteLine(result);
+
+// Циклы могут быть вложены друг в друга.
+
+for (int i = 2; i <=10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
